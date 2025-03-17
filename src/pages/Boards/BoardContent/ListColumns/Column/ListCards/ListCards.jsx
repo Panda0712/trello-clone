ec63f60d9001468f7c9780c247b878ced480d86f
Pham Tuan <tuan.pham.super@gmail.com>
2025-03-17 11:20:29 +0700

    format code structure board content p2

---
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -0,0 +1,40 @@
+import { Box } from "@mui/material";
+import CustomCard from "~/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card";
+
+const ListCards = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 1,
+        p: "0 5px",
+        m: "0 5px",
+        overflowX: "hidden",
+        overflowY: "auto",
+        maxHeight: (theme) =>
+          `calc(
+${theme.trelloCustom.boardContentHeight} - 
+${theme.spacing(5)} - 
+${theme.trelloCustom.columnHeaderHeight} - 
+${theme.trelloCustom.columnFooterHeight})`,
+        "&::-webkit-scrollbar-thumb": {
+          backgroundColor: "#ced0da",
+        },
+        "&::-webkit-scrollbar-thumb:hover": {
+          backgroundColor: "#bfc2cf",
+        },
+      }}
+    >
+      <CustomCard />
+      <CustomCard temporaryHideMedia />
+      <CustomCard temporaryHideMedia />
+      <CustomCard temporaryHideMedia />
+      <CustomCard temporaryHideMedia />
+      <CustomCard temporaryHideMedia />
+      <CustomCard temporaryHideMedia />
+    </Box>
+  );
+};
+
+export default ListCards;
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,9 +1,9 @@
 // Board Details
 
 import { Container } from "@mui/material";
-import BoardBar from "./BoardBar";
-import BoardContent from "./BoardContent";
-import AppBar from "~/components/AppBar";
+import BoardBar from "./BoardBar/BoardBar";
+import BoardContent from "./BoardContent/BoardContent";
+import AppBar from "~/components/AppBar/AppBar";
 
 const Board = () => {
   return (
