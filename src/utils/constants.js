17a3dad2aebb4abced47b3f4df3a23dd08793644
Pham Tuan <tuan.pham.super@gmail.com>
2025-03-25 00:16:11 +0700

    fix bug

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,9 @@
 // constants
-// export const API_ROOT = "http://localhost:8017";
-export const API_ROOT = "https://trello-backend-wkjl.onrender.com";
+let apiRoot = "";
+if (process.env.BUILD_MODE === "dev") {
+  apiRoot = "http://localhost:8017";
+}
+if (process.env.BUILD_MODE === "production") {
+  apiRoot = "https://trello-backend-wkjl.onrender.com";
+}
+export const API_ROOT = apiRoot;
