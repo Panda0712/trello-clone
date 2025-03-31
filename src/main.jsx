7d5ac47115b10421f853eb8210e2e5adf41650f8
Pham Tuan <tuan.pham.super@gmail.com>
2025-03-31 17:53:05 +0700

    last commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import CssBaseline from "@mui/material/CssBaseline";
-import "react-toastify/dist/ReactToastify.css";
 import { GlobalStyles, NoSsr } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
 import { ConfirmProvider } from "material-ui-confirm";
 import React from "react";
@@ -8,6 +7,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "~/App.jsx";
--- src/socketClient.js
+++ src/socketClient.js
@@ -1,3 +1,4 @@
 import { io } from "socket.io-client";
 import { API_ROOT } from "~/utils/constants";
+// create a socket io instance to interact with the base url
 export const socketIoInstance = io(API_ROOT);
